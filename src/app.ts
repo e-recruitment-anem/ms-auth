23a2cbcd145934ca800e04fbd4f3dbf2b7215cf3
aymenzitouni <a.zitouni@esi-sba.dz>
2022-04-07 16:21:22 +0100

    init project

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import compression from "compression";
+import cors from "cors";
+import morgan from "morgan";
+import * as dotenv from "dotenv";
+import { Router } from "./routes";
+
+const app = express();
+dotenv.config();
+// const isProduction = process.env.NODE_ENV === 'production'
+
+// Logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(compression());
+app.use(cors());
+app.use("/api", Router);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server is listening to port ${PORT}`);
+});
---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import { getHello } from '../controllers';
+
+const router = express.Router();
+
+router.get('/', getHello);
+
+export { router as Router };
